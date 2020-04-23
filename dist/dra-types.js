754f010321732ad58b9b9c90d46754504160f33e
Terry Crowley <tcrowley@alum.mit.edu>
2020-04-23 12:40:45 -0700

    Added geojson bucket.

--- dist/dra-types.js
+++ dist/dra-types.js
@@ -130,6 +130,8 @@ exports.BucketMap = {
     'default': 'dev-dra-us-west-2-723398989493',
     'development': 'dev-dra-us-west-2-723398989493',
     'production': 'dra-us-west-2-723398989493',
+    'state': 'dra-us-west-2-723398989493',
+    'state-dev': 'dev-dra-us-west-2-723398989493',
     'logs': 'dra-uswest-logs',
     'memsqs': 'dra-uswest-memsqs',
     'images': 'dra-uswest-images',
@@ -139,6 +141,8 @@ exports.BucketMap = {
     'splits-dev': 'dra-block-cache-dev',
     'counties': 'dra-county-cache',
     'counties-dev': 'dra-county-cache',
+    'geojson': 'dra-uswest-geojson',
+    'geojson-dev': 'dra-uswest-geojson',
 };
 
 
--- lib/bucketmap.ts
+++ lib/bucketmap.ts
@@ -3,6 +3,8 @@ export const BucketMap: any =
     'default': 'dev-dra-us-west-2-723398989493',
     'development': 'dev-dra-us-west-2-723398989493',
     'production': 'dra-us-west-2-723398989493',
+    'state': 'dra-us-west-2-723398989493',
+    'state-dev': 'dev-dra-us-west-2-723398989493',
     'logs': 'dra-uswest-logs',
     'memsqs': 'dra-uswest-memsqs',
     'images': 'dra-uswest-images',
@@ -12,4 +14,6 @@ export const BucketMap: any =
     'splits-dev': 'dra-block-cache-dev',
     'counties': 'dra-county-cache',
     'counties-dev': 'dra-county-cache',
+    'geojson': 'dra-uswest-geojson',
+    'geojson-dev': 'dra-uswest-geojson',
   }
