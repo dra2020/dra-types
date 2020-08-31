bb7282355fa615bcfdc8ed3325fbf1d83432180a
Terry Crowley <tcrowley@alum.mit.edu>
2020-08-31 12:01:07 -0700

    Add UserLayer structure used in map data.

--- lib/layer.ts
+++ lib/layer.ts
@@ -1,4 +1,4 @@
-export interface UserLayer
+export interface UserLayerMeta
 {
   createdBy: string,
   id: string,
@@ -10,4 +10,19 @@ export interface UserLayer
   deleted?: boolean,
   published?: string,
   official?: boolean
+}
+
+export type UserLayerRef = {
+  id: string,
+  fill: boolean,
+  stroke: boolean,
+  label: boolean
+  strokeColor: string,
+  strokeOpacity: number,
+  fillColor: string,
+  fillOpacity: number,
+}
+
+export type UserLayerRefIndex = {
+  [id: string]: UserLayerRef
 }
