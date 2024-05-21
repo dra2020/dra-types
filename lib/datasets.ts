d7900c78dae09197c81a1b9bf4011955370ec3d7
Terry Crowley <tcrowley@alum.mit.edu>
2024-05-21 08:54:52 -0700

    Add colorBySum to DatasetField

--- lib/datasets.ts
+++ lib/datasets.ts
@@ -5,8 +5,9 @@ export interface DatasetField
   longCaption: string,   // Can be longer descriptive phrase, especially for combinations
   order?: number,        // For ordering fields in UI
   isCombo?: boolean,     // Built-in, for census combos
-  colorBy?: boolean,     // For extended datasets
-  invert?: boolean,      // For extended datasets, colorby inverted
+  colorBy?: boolean,     // For extended datasets, display in UI
+  invert?: boolean,      // For extended datasets, colorby intensity inverted
+  colorBySum?: boolean,  // For extended datasets, colorby sums up all fields up to this one (e.g. to show "Poverty Level")
   sumOf?: string[],      // Only for import processing
 }
 export function sortFields(f1: DatasetField, f2: DatasetField): number
